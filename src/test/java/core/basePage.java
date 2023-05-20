a0ea7cee7b8e33544bc7e4d8616ce0888eff6067
thuan05 <thuan123qwe@gmail.com>
2023-05-20 19:21:54 +0700

    20/05/2023

--- src/test/java/core/basePage.java
+++ src/test/java/core/basePage.java
@@ -1,7 +1,6 @@
 package core;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -13,7 +12,5 @@ public class basePage {
 
     public basePage(WebDriver driver){
         this.driver = driver;
-//        wait = new WebDriverWait(driver, Duration.ofSeconds(10), 100);
-        PageFactory.initElements(driver, this);
     }
 }
